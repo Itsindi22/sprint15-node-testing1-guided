34e62ca0b2baa56a7a3feb5854069382daab2f8c
Inderpreet <inderpreet2004123@gmail.com>
2025-11-10 17:53:44 -0500

    test wip

--- car/car.js
+++ car/car.js
@@ -1,1 +1,15 @@
 // Build a Car class!
+
+ class Car {
+    constructor(make, model) {
+        this.make = make
+        this.model =model
+        this.odometer = 0
+    }
+    drive () {
+        
+    }
+}
+
+
+module.exports = Car
--- car/car.spec.js
+++ car/car.spec.js
@@ -1,1 +1,55 @@
+const  Car = require ('./car')
+function foo () {
+    return 'the foo'
+}
+
+
 // Test away!
+describe('our first  tests', () => {
+    test ('sanity', () => {
+    expect(5).toBe(5)
+    expect(5).toBe( 3+2)
+    expect(5).toBeDefined()
+    expect(5).toBeGreaterThan(4)
+
+     })
+     test('objects',() => {
+        const o = {a: 1}
+        const oo = {a: 1}
+        const ooo = oo
+        expect(o).toBe(o)
+        expect(oo).toBe(ooo)
+
+     }) 
+     test ('objects again', () => {
+        expect({a:1}).toEqual({a:1})
+     })
+})
+describe('foo function', () => { 
+    
+    test('foo returns the foo', () => {
+    expect(foo()).toBe('the foo')
+    expect(foo()).toHaveLength(7)
+    })
+})
+describe('Car class',()=> {
+    test('it is defined',()=> {
+       expect(Car).toBeDefined()
+        expect(Car).toBeInstanceOf(Function)
+    })
+    test ('has model and make ',() => {
+        const prius = new Car ('toyota','prius')
+        expect(prius).toHaveProperty('make','toyota')
+        expect(prius).toHaveProperty('model','prius')
+        expect(prius.make).toBeDefined()
+        expect(prius.model).toBeDefined()
+        expect(prius.make).toBe('toyota')
+        expect(prius.model).toBe('prius')
+        expect(prius).toMatchObject({make:'toyota', model: 'prius'})
+
+    })
+    test('new cars starts with the odometer at zero',() => {
+        const prius = new Car ('toyota','prius')
+        expect(prius).toHaveProperty('odometer',0)
+    })
+})
